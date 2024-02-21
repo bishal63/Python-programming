d4fc484cef8dc792fc42ee975c0ce9e0c42ee3ec
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:44:10 +0600

    mahabub

---
+++ turtle ar karbar.py
@@ -0,0 +1,32 @@
+'''import turtle
+turtle.color("red","yellow")
+turtle.begin_fill()
+while True:
+    turtle.forward(200)
+    turtle.left(170)
+    if abs(turtle.position())<1:
+        break
+turtle.end_fill()
+turtle.done()
+
+import turtle
+turtle.color("red","yellow")
+turtle.begin_fill()
+while True:
+    turtle.forward(400)
+    turtle.left(170)
+    if abs(turtle.position())<1:
+        break
+turtle.end_fill()
+turtle.done()
+
+import turtle
+turtle.color("green","gray")
+turtle.begin_fill()
+while True:
+    turtle.forward(200)
+    turtle.left(170)
+    if abs(turtle.position())<1:
+        break
+turtle.end_fill()
+turtle.done()
