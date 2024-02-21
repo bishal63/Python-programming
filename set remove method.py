8661c4aaf420cce8e7dc980d79cccdcd5e864c2f
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:38:18 +0600

    mahabub

---
+++ set remove method.py
@@ -0,0 +1,25 @@
+#set ar kono item badh dewar jonno
+#remove method
+a={1,3,6,4,7,6}
+a.remove(6)
+print(a)
+
+#discard method
+a={1,3,6,4,7,6}
+a.discard(3)
+print(a)
+
+#discard method(oi item na thaklew error dekhay na)
+a={1,3,6,4,7,6}
+a.discard(10)
+print(a)
+
+#pop method(jekono item badh dewar jonno)
+a={1,3,6,4,7,6}
+a.pop()
+print(a)
+
+#clear method(shokal item badh dewar jono
+a={1,3,6,4,7,6}
+a.clear()
+print(a)
