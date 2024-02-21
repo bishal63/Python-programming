6bdc25f1fb5ae31054cc06d69a0c7067ce857d59
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:40:32 +0600

    mahabub

---
+++ string formatting.py
@@ -0,0 +1,11 @@
+#format string ninnoy jekhane format hisabe f use kora hoy
+a=38
+b=77
+Name="Mahabub"
+print(f"this is my name {a+b}")
+#dui ta variable ar jonno
+num1=16
+num2=45
+Name="Mahabub"
+print(f"this is my super number {50+50}",num1+num2)
+print(f"my name is {Name}",num1+num2)
