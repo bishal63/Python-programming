b329d5d1aecf0bb2f1004fc5089cfdeff145d7ef
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:31:40 +0600

    mahabub

---
+++ operator(assinment).py
@@ -0,0 +1,29 @@
+'''Assingment operator
+Addition ar jonno'''
+a=10
+a=a+30
+print(a)
+#subtraction ar jonno
+a=30
+a=a-20
+print(a)
+#multiplication ar jonno
+a=10
+a=a*10
+print(a)
+#division ar jonno
+a=10
+a=a/5
+print(a)
+#modulas ar jonno
+a=25
+a=a%2
+print(a)
+#exponentation ar jonno
+a=10
+a=a**2
+print(a)
+#floor division ar jonno
+a=25
+a=a//2
+print(a)
