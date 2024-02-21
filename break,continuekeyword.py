707757fd48b86214b35aa9c8da7ab8af57551d8b
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:00:19 +0600

    mahabub

---
+++ break,continuekeyword.py
@@ -0,0 +1,11 @@
+'''break statement use kori
+for name in range(9):
+    if name==6:
+        break
+    print(name)'''
+
+#continue statement use kori
+for name in range(9):
+    if name==6:
+        continue
+    print(name)
