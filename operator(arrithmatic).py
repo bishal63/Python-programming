b0b8048c4dd272f501147672726b98e3444182a5
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:31:06 +0600

    mahabub

---
+++ operator(arrithmatic).py
@@ -0,0 +1,29 @@
+'''Operator ninnoy korar jonno
+Addition operator'''
+a=58
+b=42
+print(a+b)
+#subtraction operator
+a=34
+b=12
+print(a-b)
+#multiplication operator
+a=12
+b=13
+print(a*b)
+#division operator
+a=7
+b=3
+print(a/b)
+#modulas operator
+a=45
+b=22
+print(a%b)
+#exponnentation operator
+a=5
+b=3
+print(a**b)
+#floor division operator
+a=15
+b=7
+print(a//b)
