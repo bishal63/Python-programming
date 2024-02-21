65cacfc52e162ca2cba093b3e3c81b35580cdcaa
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:21:33 +0600

    mahabub

---
+++ iterator.py
@@ -0,0 +1,11 @@
+li=['Mahabub','Bishal','Saikat','Shahadat','Mahim','shuvo','Alif','Hamim']
+x=iter(li)
+print(next(x))
+
+li=['Mahabub','Bishal','Saikat','Shahadat','Mahim','shuvo','Alif','Hamim']
+y=iter(li)
+print(next(y))
+
+li=['Mahabub','Bishal','Saikat','Shahadat','Mahim','shuvo','Alif','Hamim']
+a=iter(li)
+print(next(a))
