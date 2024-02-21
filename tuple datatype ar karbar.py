521a28875b3378847097f3fc25105bd71033729e
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:41:55 +0600

    mahabub

---
+++ tuple datatype ar karbar.py
@@ -0,0 +1,21 @@
+#1.tuple datatype toiri kori
+name=("mahabub","alam","bishal","mahfuj","alam","mahim")
+print(name)
+
+name=("mahabub","alam","bishal","mahfuj","alam","mahim")
+print(type(name))
+
+name=("mahabub","alam","bishal","mahfuj","alam","mahim")
+print(name[0])
+
+name=("mahabub","alam","bishal","mahfuj","alam","mahim")
+a,b,c,d,e,f=name
+print(c)
+
+name=("mahabub",["ami","tumi","shea"],{"alam","bishal"},1,2,3,4,5)
+for names in name:
+    print(names,type(names))
+
+name=("mahabub","alam","bishal","mahfuj","alam","mahim")
+print(name.count("alam"))
+
