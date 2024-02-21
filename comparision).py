12dfe397deca0beb4a2876ee33522e11be88d774
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:01:29 +0600

    mahabub

---
+++ comparision).py
@@ -0,0 +1,25 @@
+'''relational/comparision operator ninnoy
+(==)equal ar jonno'''
+a=10
+b=12
+print(a==b)
+#(>)greater than ar jonno
+a=68
+b=57
+print(a>b)
+#(<)less than ar jonno
+a=56
+b=78
+print(a<b)
+#(<=)greater than equal ar jonno
+a=89
+b=78
+print(a<=b)
+#(<=)less than equal ar jonno
+a=56
+b=67
+print(a>=b)
+#(!=)not equal ar jonno
+a=57
+b=58
+print(a!=b)
