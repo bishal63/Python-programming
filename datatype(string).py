becc8528767df6c7e856e6168d9f6837cf3b2ad6
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:09:17 +0600

    mahabub

---
+++ datatype(string).py
@@ -0,0 +1,10 @@
+#string type data ninnoy
+Name="Mahabub"
+print(Name)
+#ata kon type data ta ninnoy?
+Name="Mahabub"
+print(type(Name))
+#extra string type data ninnoy
+Name="Mahabub"
+print("My name is" + ' '+ Name)
+
