f6436f4bb291c1f5cf5e5622e585777524209f4e
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:13:28 +0600

    mahabub

---
+++ dictionary item remove.py
@@ -0,0 +1,36 @@
+#pop method use kore kono item remove kora
+dictionary={
+    "mahabub":{
+        "name":"mahabub",
+        "profession":"student",
+        "religion":"islam",
+        "distric":"dinajpur"
+    },
+    "saikat":{
+        "name":"saikat",
+        "roll":3,
+        "class":"five",
+        "thana":"parbotipur"
+    }
+}
+#dictionary.pop("mahabub")
+#print(dictionary)
+
+
+#popitem method use kore sesher item remove kora
+dictionary={
+    "mahabub":{
+        "name":"mahabub",
+        "profession":"student",
+        "religion":"islam",
+        "distric":"dinajpur"
+    },
+    "saikat":{
+        "name":"saikat",
+        "roll":3,
+        "class":"five",
+        "thana":"parbotipur"
+    }
+}
+#dictionary.popitem()
+#print(dictionary)
