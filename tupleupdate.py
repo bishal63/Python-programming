03deffad4e030266b23d50415e2c4880b694e384
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:43:48 +0600

    mahabub

---
+++ tupleupdate.py
@@ -0,0 +1,22 @@
+#tuple update make kori
+thistuple=('mahabu','alam','bishal')
+hridita=list(thistuple)
+hridita.append('hridoy')
+thistuple=tuple(hridita)
+print(thistuple)
+
+
+#tuple updaate
+mtuple=('mahabub','alam','bishal')
+a=list(mtuple)
+a.append('saikat')
+mtuple=tuple(a)
+print(mtuple)
+
+
+#tuple update kori
+hridita=('mahabub','alam','bishal')
+a=list(hridita)
+a.append('saikat')
+hridita=tuple(a)
+print(hridita)
