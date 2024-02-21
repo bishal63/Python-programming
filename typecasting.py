e9194e185fc1d750e20920ef34a346e300124a79
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:44:34 +0600

    mahabub

---
+++ typecasting.py
@@ -0,0 +1,15 @@
+'''typecasting ninnoy(ak type data thekhe onno type datay rupantor)
+integer type data ke float a rupantor oo type ninnoy'''
+a=1342
+print(float(a))
+print(type(a))
+#integer type data ke complex a rupantor oo type ninnoy
+a=1342
+print(complex(a))
+print(type(a))
+'''onnanno type data gulor man invalid
+orthat acceptable man noy ba type casting a rupantor kora jay na'''
+
+a=2
+print(float(a))
+
