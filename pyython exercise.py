57e4e0aa9a88d046eebb003fc1c8ffe73076e999
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:33:40 +0600

    mahabub

---
+++ pyython exercise.py
@@ -0,0 +1,56 @@
+#recursion function exercise
+'''def ReFun():
+    print("mahabub")
+    ReFun()
+ReFun()'''
+
+"""def ReFun():
+    print("hridita")
+    ReFun()
+ReFun()"""
+
+'''def ReFun():
+    print("bishal")
+    ReFun()
+ReFun()'''
+
+#zip function exercise
+'''name=["mahabub","alam","bishal","hridita","nowshin","sinha"]
+number=[1,2,3,4,5,6]
+print(list(zip(name,number)))'''
+
+'''car_name=["tata","mercidies","rolex","audi"]
+car_price=[12484564,85746545,85764534,2543443765]
+print(list(zip(car_name,car_price)))'''
+
+#lambda function exercise
+'''c=lambda a,b:a-b
+print(c(30,20))'''
+
+'''y=lambda a,z:a*z
+print(y(10,20))'''
+
+'''z=lambda q,w:q+w
+print(z(30,40))'''
+
+#function exercise
+'''def mahabub(a,b):
+    y=a+b
+    print(y)
+mahabub(10,30)'''
+
+'''def bishal(c,d):
+    v=c*d
+    print(v)
+bishal(20,5464)'''
+
+'''def hridita(z,x):
+    sum=z/x
+    print(sum)
+hridita(7265345,387)'''
+
+#;list oo list  ar method exercise
+name=["mahabub","alam","bishal","hridita","nowshin","sinha"]
+print(name)
+print(type(name))
+name[]
