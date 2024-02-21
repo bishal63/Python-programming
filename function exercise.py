35079ef772366d2b74e0ca8669dfd135b4775ab2
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:17:54 +0600

    mahabub

---
+++ function exercise.py
@@ -0,0 +1,20 @@
+'''def mahabub(a,b):
+    multiple=a*b
+    print(multiple)
+mahabub(10,5)'''
+
+def mahabub(c,d):
+    sum=c/d
+    math=c*d
+    print(sum)
+    print(math)
+mahabub(80,20)
+
+def mahabub(a,b):
+    y=a+b
+    print(y)
+    y=a*b
+    print(y)
+
+mahabub(10,18)
+
