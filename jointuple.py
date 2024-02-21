6c9366ba9055fbfde60bbd70ed46e57806e85529
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:22:44 +0600

    mahabub

---
+++ jointuple.py
@@ -0,0 +1,14 @@
+#join tuple(dui ta tuple list ke akti tuple list a rupantor kori)
+thistuple=('mahabu','alam','bishal')
+thislist = ("apple", "banana", "cherry")
+name=thistuple+thislist
+print(name)
+
+#aivabew kora jay
+thistuple=('mahabu','alam','bishal')
+thislist = ("apple", "banana", "cherry")
+print(thistuple+thislist)
+
+#multipy tuple list
+thistuple=('mahabu','alam','bishal')
+print(thistuple*3)
