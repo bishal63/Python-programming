3f6e420c9978895967a4a10c91ad5f3199e43e5a
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:15:19 +0600

    mahabub

---
+++ extendlist.py
@@ -0,0 +1,11 @@
+#extand list(dui ta list ke ak kori)
+thislist = ["apple", "banana", "cherry"]
+num=[1,5,6,10]
+thislist.extend(num)
+print(thislist)
+
+#abar aivabew kora jaw
+thislist = ["apple", "banana", "cherry"]
+num=[1,5,6,10]
+thislistnum=thislist+num
+print(thislistnum)
