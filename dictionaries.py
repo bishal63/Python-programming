b42aabf1e57e912ffa373406dcdfa2d34326a08f
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:11:08 +0600

    mahabub

---
+++ dictionaries.py
@@ -0,0 +1,17 @@
+#dictionary datatype
+name={
+    'bishal':{
+     'name':'bishal',
+     'class':'twelve',
+     'roll':'2',
+     'section':'science'
+    },
+    'mahabub':{
+        'location':'dhaka',
+        'nation':'bangladeshi',
+        'religion':'islam'
+    }
+}
+#print(name)
+#print(name['bishal']
+print(name['bishal']['roll'])
