8e7da45bdb37176ab7ff56bd4f4903f36df7558d
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:39:36 +0600

    mahabub

---
+++ string data type ar project.py
@@ -0,0 +1,48 @@
+bishal="I love programming"
+print(bishal)
+
+bishal="I love programming"
+print(bishal.upper())
+
+bishal="I love programming"
+print(bishal.lower())
+
+bishal="i love programming"
+print(bishal.capitalize())
+
+bishal="I love programming"
+print(len(bishal))
+
+bishal="I love love programming"
+print(bishal.count("love"))
+
+bishal="I love programming"
+print(bishal.find("I"))
+
+bishal="I love programming"
+print(bishal[7])
+
+bishal="I love programming"
+print(bishal.replace("I","we"))
+
+bishal="I love programming"
+print(bishal.split())
+
+bishal="I love programming"
+print(bishal.startswith("I"))
+
+bishal="I love programming"
+print(bishal.endswith("p"))
+
+bishal=" I love programming "
+print(bishal.strip())
+
+
+bishal=" I love programming"
+print(bishal.lstrip())
+
+bishal=" I love programming  "
+print(bishal.rstrip())
+
+bishal="mahabub" + "alam" + "bishal"
+print(bishal)
