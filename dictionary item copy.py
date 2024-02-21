5095e8a6f98dfb40479cfdb81b1cd26dad37f878
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:12:59 +0600

    mahabub

---
+++ dictionary item copy.py
@@ -0,0 +1,18 @@
+#dictionary datatype ke copy kori
+dictionary={
+    "mahabub":{
+        "name":"mahabub",
+        "profession":"student",
+        "religion":"islam",
+        "distric":"dinajpur"
+    },
+    "saikat":{
+        "name":"saikat",
+        "roll":3,
+        "class":"five",
+        "thana":"parbotipur"
+    }
+}
+name=dictionary.copy()
+print(name)
+print(dictionary)
