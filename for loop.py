e95c7fcc6ba6bc1e093db8dc3cbde2c82ee604a4
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:16:44 +0600

    mahabub

---
+++ for loop.py
@@ -0,0 +1,17 @@
+'''for mahabub in range(101):
+    alam="I Love Programming"
+    print(mahabub,".",alam)'''
+'''list=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+for mahabub in list:
+    print(mahabub)'''
+
+'''list=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+for mahabub in range(26):
+    print(mahabub,list[mahabub])'''
+'''n=int(input("number"))
+for i in range(1,11):
+    print("{}*{}={}".format(n,i,n*i))
+
+number=int(input("number:"))
+for i in range(1,11):
+    print("{}*{}={}".format(number,i,number*i))'''
