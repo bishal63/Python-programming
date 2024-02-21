f069d59d355b6bb4f1620d414d176b659a3e5a77
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:05:43 +0600

    mahabub

---
+++ create_own_module.py
@@ -0,0 +1,20 @@
+class muksadul:
+    def __init__(self,age,address,nationality):
+        self.age=age
+        self.address=address
+        self.nationality=nationality
+    def all(self):
+        print(self.age)
+        print(self.address)
+        print(self.nationality)
+class mahabub:
+    def __init__(self,age,address,nationality,religion):
+        self.age=age
+        self.address=address
+        self.nationality=nationality
+        self.religion=religion
+    def all(self):
+        print(self.age)
+        print(self.address)
+        print(self.nationality)
+        print(self.religion)
