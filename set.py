8f1edbd1a86a7ffe2f649260c12ed3311555d6cf
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:38:45 +0600

    mahabub

---
+++ set.py
@@ -0,0 +1,11 @@
+#set item ar mot index number bair kori
+thisset={'mahabub','alam','bishal'}
+print(type(thisset))
+
+#mot index number(string ar jono)
+m={'mahabub','alam','bishal'}
+print(len(m))
+
+#mot index number(number ar jonno)
+m={1,3,6,5,7}
+print(len(m))
