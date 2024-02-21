9cec2649f4bbe53bb131a978a523f0ebc7ca36a6
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:26:50 +0600

    mahabub

---
+++ loop exercise.py
@@ -0,0 +1,28 @@
+'''a=[9,8,6,4,3]
+for b in range (len(a)):
+    print(b)'''
+
+'''b=["mahabub","alam","bishal"]
+for i in b:
+    print(i)'''
+
+'''b=["mahabub","alam","bishal"]         
+for i in range(len(b)):
+    print(i)'''
+
+'''b=["mahabub","alam","bishal"]
+i=0
+while i<len(b):
+    print(b[i])
+    i=i+1'''
+
+'''b=['mahabub','alam','bishal']
+a=0
+while a<len(b):
+    print(b[a])
+    a=a+1'''
+alam=['mahfuj','alam','mahim']
+mahabub=0
+while mahabub<len(alam):
+    print(alam[mahabub])
+    mahabub=mahabub+1
