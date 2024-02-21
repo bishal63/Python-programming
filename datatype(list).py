eafc08de6dd84cf98b0e720c8f8963e5bab1203e
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:07:17 +0600

    mahabub

---
+++ datatype(list).py
@@ -0,0 +1,10 @@
+#list type data ninnoy
+li=['Mahabub','Bishal','Saikat','Shahadat','Mahim','shuvo','Alif','Hamim']
+print(li)
+#ata kon type data ta ninnoy
+li=['Mahabub','Bishal','Saikat','Shahadat','Mahim','shuvo','Alif','Hamim']
+print(type(li))
+#list data mutable(poriborton hoy)
+li=['Mahabub','Bishal','Saikat','Shahadat','Mahim','shuvo','Alif','Hamim']
+li[2]='Hablu'
+print(li)
