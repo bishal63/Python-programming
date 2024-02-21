f0fb01698c2144e8b75acb97a7b3f4dd611f5023
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:14:53 +0600

    mahabub

---
+++ exercise.py
@@ -0,0 +1,132 @@
+'''class mahabub:
+    name="alam"
+    roll=3
+    zila="dinajpur"
+
+bishal=mahabub()
+print(bishal.name)
+
+class bishal:
+    name="bishal"
+    zila="dinajpur"
+    profession="dinajpur"
+
+mahabub=bishal()
+alam=bishal()
+print(mahabub.zila)
+print(alam.name)'''
+
+'''class sum:
+    a=10
+    b=15
+    c=20
+d=sum()
+e=sum()
+print(d.c)'''
+
+'''class mahabub:
+    a=10
+    b=18
+    c=20
+d=mahabub()
+print(d.a)'''
+
+'''a=[10,20,30,40,50]
+print(a)'''
+'''a=[10,20,30,40,50]
+a[1]=15
+print(a)'''
+
+'''a=[10,20,30,40,50]
+a.append(60)
+print(a)'''
+'''a=[10,20,30,40,50]
+a.insert(2,25)
+print(a)'''
+
+'''a=[10,20,30,40,50]
+a.clear()
+print(a)'''
+
+'''a=[10,20,30,40,50]
+a.remove(20)
+print(a)'''
+
+'''a=[10,20,30,40,50]
+a.pop(3)
+print(a)'''
+
+'''a=[10,20,30,40,50]
+for number in a:
+    print(number)'''
+
+'''a=[10,20,30,40,50]
+for number in range(len(a)):
+    print(number)
+
+a=[10,20,30,40,50]
+i=0
+while i<len(a):
+    print(i)
+    i=i+1
+a=[10,20,30,40,50]
+print([i+3 for i in a])
+
+a=[10,20,30,40,50]
+y=[i+10 for i in a]
+print(y)
+
+a=[10,20,30,40,50]
+a.sort()
+print(a)
+
+a=[10,20,30,40,50]
+a.sort(reverse=True)
+print(a)
+
+a=[10,20,30,40,50]
+number=[y+5 for y in a]
+print(number)
+
+a=[10,20,30,40,50]
+b=a.copy()
+print(a)
+print(b)
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+a=[10,20,30,40,50]
+thislist.extend(a)
+print(thislist)
+
+a=[10,20,30,40,50]
+b=a.copy()
+print(a)
+print(b) 
+a=[10,20,30,40,50]
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+a.extend(thislist)
+print(a)
+
+thislist = [["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"],
+           [10,20,30,40,50],
+           ["apple", "banana", "cherry"]]
+print(thislist[1][1])'''
+
+
+
+'''thislist = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thislist[3:6])
+
+thislist =("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thislist.index("cherry"))
+
+thislist =("apple","cherry", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thislist.count("cherry"))'''
+
+a=("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+b=(10,20,30,40,50)
+print(a+b)
+
+
+
+
