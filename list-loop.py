689de3754eba33ca3443eeb558742ba40389379b
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:25:49 +0600

    mahabub

---
+++ list-loop.py
@@ -0,0 +1,17 @@
+#for loop use kore list make kori
+thislist = ["apple", "banana", "cherry"]
+for name in thislist:
+    print(name)
+
+
+#range()len()  loop use kore list make kori
+thislist = ["apple", "banana", "cherry"]
+for name in range(len(thislist)):
+    print(thislist[name])
+
+  #while loop use kore list make kori
+thislist = ["apple", "banana", "cherry"]
+i=0
+while i<len (thislist):
+    print(thislist[i])
+    i=i+1
