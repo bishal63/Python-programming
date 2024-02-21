83ea0f53a2752da9ea63929fef06566a7fe06adb
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:13:56 +0600

    mahabub

---
+++ dictionary loop.py
@@ -0,0 +1,71 @@
+#dictionary datatype for loop use kori
+dictionary={
+    "mahabub":{
+        "name":"mahabub",
+        "profession":"student",
+        "religion":"islam",
+        "distric":"dinajpur"
+    },
+    "saikat":{
+        "name":"saikat",
+        "roll":3,
+        "class":"five",
+        "thana":"parbotipur"
+    }
+}
+#for peapole in dictionary:
+    #print(peapole)
+
+#loop ae moddhe key method ues kori
+dictionary={
+    "mahabub":{
+        "name":"mahabub",
+        "profession":"student",
+        "religion":"islam",
+        "distric":"dinajpur"
+    },
+    "saikat":{
+        "name":"saikat",
+        "roll":3,
+        "class":"five",
+        "thana":"parbotipur"
+    }
+}
+#for peapole in dictionary.keys():
+    #print(peapole)
+
+#loop ae moddhe value method ues kori
+dictionary={
+    "mahabub":{
+        "name":"mahabub",
+        "profession":"student",
+        "religion":"islam",
+        "distric":"dinajpur"
+    },
+    "saikat":{
+        "name":"saikat",
+        "roll":3,
+        "class":"five",
+        "thana":"parbotipur"
+    }
+}
+#for peapole in dictionary.values():
+    #print(peapole)
+
+#loop ae moddhe item method ues kori
+dictionary={
+    "mahabub":{
+        "name":"mahabub",
+        "profession":"student",
+        "religion":"islam",
+        "distric":"dinajpur"
+    },
+    "saikat":{
+        "name":"saikat",
+        "roll":3,
+        "class":"five",
+        "thana":"parbotipur"
+    }
+}
+for peapole in dictionary.items():
+    print(peapole)
