2489c59ad3a843c1417b07efb10d646c696b9d17
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:32:38 +0600

    mahabub

---
+++ practise loop list.py
@@ -0,0 +1,17 @@
+#for loop:
+thislist = ["apple", "banana", "cherry"]
+for name in thislist:
+  print(name)
+
+  #while loop
+  thislist = ["apple", "banana", "cherry"]
+  for name in range(len(thislist)):
+    print(thislist[name])
+
+
+  #while loop
+  li = ['Mahabub', 'Bishal', 'Saikat', 'Shahadat', 'Mahim', 'shuvo', 'Alif', 'Hamim']
+  i=0
+  while i<len(li):
+    print(li[i])
+    i=i+1
