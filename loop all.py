40385829ad45edbcdbf8ab218379637af091dd49
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:26:17 +0600

    mahabub

---
+++ loop all.py
@@ -0,0 +1,26 @@
+'''list=[1,2,3,4,5,6,7,8,9,10]
+for list1 in list:
+    print(list1)'''
+
+#list=[1,2,3,4,5,6,7,8,9,10]
+'''for list1 in range(1,11,1+1):
+    print(list1)'''
+
+'''list=[1,2,3,4,5,6,7,8,9,10]
+for list1 in list:
+    print(list1)'''
+
+'''bishal=['mahabub','alam','bishal']
+for i in bishal:
+    print(i)'''
+
+bishal=['mahabub','alam','bishal']
+for i in range(len(bishal)):
+    print(bishal[i])
+
+bishal=['mahabub','alam','bishal']
+for i in range(len(bishal)):
+    print(bishal[i])
+
+
+
