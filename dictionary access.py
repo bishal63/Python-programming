f5005b6d87c707ad66bae777495f98bf25701bed
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:11:29 +0600

    mahabub

---
+++ dictionary access.py
@@ -0,0 +1,18 @@
+#dictionary te kono item acces kora
+dictionary={
+    "mahabub":{
+        "name":"mahabub",
+        "profession":"student",
+        "religion":"islam",
+        "distric":"dinajpur"
+    },
+    "saikat":{
+        "name":"saikat",
+        "roll":3,
+        "class":"five",
+        "thana":"parbotipur"
+    }
+}
+print(dictionary.get("mahabub"))
+#print(dictionary.keys())
+print(dictionary.values())
