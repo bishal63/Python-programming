0281f9d1b0137c6cadd0f548ab9d52bf10a5b75f
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:09:39 +0600

    mahabub

---
+++ datatype(tuple).py
@@ -0,0 +1,6 @@
+#tuple data type ninnoy
+tup=('Mahabub','Bishal','Saikat','Shahadat','Mahim','shuvo','Alif','Hamim')
+print(tup)
+#ata kon type data ta ninnoy
+tup=('Mahabub','Bishal','Saikat','Shahadat','Mahim','shuvo','Alif','Hamim')
+print(type(tup))
