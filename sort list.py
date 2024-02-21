1ea5df51b416f5eb375a5e66dcd815babbe4ea25
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:39:08 +0600

    mahabub

---
+++ sort list.py
@@ -0,0 +1,10 @@
+#sort list(list ar item ke choto theke boro sajanor jonno)
+list=[4,1,8,6,7,3]
+list.sort()
+print(list)
+
+
+#reverse list(list ar item ke boro theke choto sajanor jonno
+list=[1,3,5,7,9,11]
+list.sort(reverse=True)
+print(list)
