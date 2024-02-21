8a5dd10b0c954994c6a804bc5024dcc695e1a9b8
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:40:57 +0600

    mahabub

---
+++ swaping.py
@@ -0,0 +1,11 @@
+#swaping ninnoy ba ak variable ar man onno variable a sthanontor
+a=16
+b=19
+a,b=b,a
+print(a,b)
+#string ar maddhome sthanontor
+a=67
+b=89
+a,b=b,a
+print('my number is',a)
+print('my number is',b)
