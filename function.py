b478fe53463ee5c62d5445c55934ef5e049586bd
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:18:29 +0600

    mahabub

---
+++ function.py
@@ -0,0 +1,38 @@
+def mul(a,b):
+    bishal=a-b
+    print(bishal)
+mul(20,30)
+
+def add(a,b):
+    bishal=a+b
+    print(bishal)
+add(30,20)
+
+def mul(a,b):
+    bishal=a*b
+    print(bishal)
+mul(10,7)
+
+def div(a,b):
+    mahabub=a/b
+    print(mahabub)
+div(24,6)
+
+def mod(a,b):
+    mahabub=a%b
+    print(mahabub)
+mod(24,6)
+
+def bishal(a,b):
+    mahabub=a//b
+    print(mahabub)
+bishal(24,5)
+
+def mahim(a,b):
+    mahabub=a**b
+    print(mahabub)
+mahim(24,2)
+
+def girlfriend():
+    print("I love you")
+girlfriend()
