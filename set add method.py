f1ea240854fdd1a6ccd7f3dfb9b3d482b145e799
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:35:11 +0600

    mahabub

---
+++ set add method.py
@@ -0,0 +1,19 @@
+#set method
+#kono item add korar jonno
+a={1,3,6,4,7,6}
+a.add(2)
+print(a)
+
+#dui ta set ke add korar jonno
+#akti set a rupantor
+a={1,3,6,4,7,6}
+b={'mahabub','alam','bishal'}
+a.update(b)
+print(a)
+
+#iterable data type
+#differnt type data ke akok set type a rupantor
+a={1,3,6,4,7,6}
+b=('Mahabub','Bishal','Saikat','Shahadat','Mahim','shuvo','Alif','Hamim')
+a.update(b)
+print(a)
