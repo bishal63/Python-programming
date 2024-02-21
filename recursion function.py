98811fc182c229783b875ead886aab6916230d43
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:34:07 +0600

    mahabub

---
+++ recursion function.py
@@ -0,0 +1,27 @@
+#recursion function use kori
+#def ReFun():
+    #print("I love you")
+    #ReFun()
+#ReFun()
+
+
+'''def ReFun():
+    print("mahabub")
+    ReFun()
+ReFun()'''
+
+'''def ReFun():
+    print("bishal")
+    
+    ReFun()
+ReFun()'''
+
+'''def ReFun():
+    print('bangladesh')
+    ReFun()
+ReFun()'''
+
+def ReFun():
+    print("mahim")
+    ReFun()
+ReFun()
