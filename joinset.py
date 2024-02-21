8e1f60b8b969c90d62da19cf42c10e58b302a886
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:22:01 +0600

    mahabub

---
+++ joinset.py
@@ -0,0 +1,16 @@
+#duiti set ke akti set a rupantor(update method use kore)
+a={1,2,3,4,5}
+b={6,7,8,9,10}
+a.update(b)
+print(a)
+
+#union method use kore
+a={1,2,3,4,5}
+b={6,7,8,9,10}
+print(b.union(a))
+
+#variable declared ar maddhome
+a={1,2,3,4,5}
+b={6,7,8,9,10}
+c=b.union(a)
+print(c)
