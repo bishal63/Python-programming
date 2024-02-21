388fd591fb990ed6c8968159c5f48d727dedda7c
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:42:22 +0600

    mahabub

---
+++ tuple method(index,count).py
@@ -0,0 +1,20 @@
+'''index method use tuple item ar index number bair kori
+peapole=('Mahabub','Bishal','Saikat','Shahadat','Mahim','shuvo','Alif','Hamim')
+print(peapole.index('Shahadat'))
+
+#variable call korar maddhome
+peapole=('Mahabub','Bishal','Saikat','Shahadat','Mahim','shuvo','Alif','Hamim')
+thislist=peapole.index('Bishal')
+print(thislist)
+
+#count method use kore kon item kotobar use hoyeche ta bair kori
+peapole=('Mahabub','Bishal','Saikat','Shahadat','Mahim','shuvo','Alif','Hamim','Saikat')
+print(peapole.count('Saikat'))
+
+#variable call korar maddhome
+peapole=('Mahabub','Bishal','Saikat','Shahadat','Mahim','shuvo','Alif','Hamim','Saikat')
+mylist=peapole.count('Saikat')
+print(mylist)'''
+
+peapole=('Mahabub','Bishal','Saikat','Shahadat','Mahim','shuvo','Alif','Hamim','Saikat')
+print(peapole.index('Hamim'))
