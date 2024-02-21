a25c433fcad4a044e6fbc45b3836d171c43d449b
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:01:07 +0600

    mahabub

---
+++ comment.py
@@ -0,0 +1,4 @@
+#code ka mone rakhar jonno comment use kora hoy(single line comment)
+print('Mahabub')
+('''code ka mone rakhar jonno comment use kora hoy oo
+akadhik line comment use korar jonno (multiline comment use kora hoy''')
