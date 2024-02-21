a601c9002ee74550ac9e8b0850499c8e7d9102f2
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:43:16 +0600

    mahabub

---
+++ tupleloop.py
@@ -0,0 +1,21 @@
+#for loop a tuple list use kori
+peapole=('Mahabub','Bishal','Saikat','Shahadat','Mahim','shuvo','Alif','Hamim')
+for name in peapole:
+    print(name)
+
+#range(len) loop a tuple list use kori
+thislist = ("apple", "banana", "cherry")
+for name in range(len(thislist)):
+    print(thislist[name])
+
+#while loop a tuple list use kori
+thislist=('mahabub','alam','bishal')
+name=0
+while name<len(thislist):
+    print(thislist[name])
+    name=name+1
+
+peapole=('Mahabub','Bishal','Saikat','Shahadat','Mahim','shuvo','Alif','Hamim')
+x=peapole.index("shuvo")
+print(x)
+
