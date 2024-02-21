85d08d01098f588b909b2158d10d7fc56d939708
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-21 21:46:04 +0600

    mahabub

---
+++ zip function.py
@@ -0,0 +1,23 @@
+'''number1=[1,2,3,4,5]
+number2=["mahabub","alam","bishal","mahfuj","mahim"]
+print(list(zip(number1,number2)))'''
+
+'''number1=(1,2,3,4,5)
+number2=(6,7,8,9,10)
+print(list(zip(number1,number2)))
+
+m=("mahabub","alam","bishal")
+h=("hridita","nowshin","sinha")
+print(tuple(zip(m,h)))'''
+
+x=lambda a,b:a+b
+print(x(20,30))
+
+y=lambda a,b:a-b
+print(y(20,10))
+
+
+number1=(1,2,3,4,5)
+number2=(6,7,8,9,10)
+print(list(zip(number1,number2)))
+
